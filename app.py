c27f93ed33964660ab4b5ddbc79c6948798c9896
grzeh-p <progre@o2.pl>
2020-09-06 11:22:05 +0200

    0.2
    dziala listowanie userow i postaci (select*)
    przy tworzeniu postaci wybieramy wlasciciela z listy zamiast zgadywac ID

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import sqlite3
 from flask import Flask, render_template
 from flask_restful import Api
-from Characters.Create_Character import CreateCharacter
+from flask_sqlalchemy import SQLAlchemy
+
+from Characters.create_character import CreateCharacter
 from Users.Create_User import UserRegister
 
-
+db = SQLAlchemy()
 app = Flask(__name__)
 api = Api(app)
 
@@ -20,22 +23,44 @@ def user_register():
 
 @app.route('/user_list')
 def user_list():
-    return render_template("user_list.html")
+    conn = sqlite3.connect("data.db")
+    conn.row_factory = sqlite3.Row
+
+    cur = conn.cursor()
+    cur.execute("select ID, USERNAME from users")
+
+    rows = cur.fetchall()
+    return render_template("user_list.html", rows=rows)
 
 
 @app.route('/char_add')
 def char_add():
-    return render_template("char_add.html")
+    conn = sqlite3.connect("data.db")
+    conn.row_factory = sqlite3.Row
+
+    cur = conn.cursor()
+    cur.execute("select ID, USERNAME from users")
+
+    rows = cur.fetchall()
+    return render_template("char_add.html", rows=rows)
 
 
 @app.route('/char_list')
 def char_list():
-    return render_template("char_list.html")
+    # return render_template("char_list.html")
+    conn = sqlite3.connect("data.db")
+    conn.row_factory = sqlite3.Row
+
+    cur = conn.cursor()
+    cur.execute("select * from characters")
+
+    rows = cur.fetchall()
+    return render_template("char_list.html", rows=rows)
 
 
 @app.route('/Roll')
 def roll_main():
-    return render_template("Roll.html")
+    return render_template("roll.html")
 
 
 api.add_resource(CreateCharacter, '/char_add')
